e3ee4e3c0986cfbcb2cae9123c847e7238332fc9
Badar Ahmed <badar.mail@gmail.com>
2016-08-02 14:47:39 -0700

    Remove unused underscore & add some error handling

--- api/resolver.js
+++ api/resolver.js
@@ -1,6 +1,5 @@
 var sync = require('synchronize');
 var request = require('request');
-var _ = require('underscore');
 
 // The API that returns the in-email representation.
 module.exports = function(req, res) {
@@ -26,23 +25,31 @@ module.exports = function(req, res) {
     return;
   }
 
-  var html = '<style>.card-label{color: white; font-size: 0.7em; border-radius:3px;-moz-box-sizing:border-box;box-sizing:border-box;display:block;float:left;height:30px;line-height:30px;margin:0 4px 4px 0;min-width:40px;padding:0 10px;width:auto;}  .card-label-green{background-color:#61bd4f}.card-label-yellow{background-color:#f2d600}.card-label-orange{background-color:#ffab4a}.card-label-red{background-color:#eb5a46} .card-label-purple{background-color:#c377e0} .card-label-blue{background-color:#0079bf} .card-label-pink{background-color:#ff80ce}.card-label-sky{background-color:#00c2e0}.card-label-lime{background-color:#51e898}.card-label-black{background-color:#4d4d4d}</style>';
-  html += '<table id="" class="card-v3" cellpadding="0" cellspacing="0" style="border:1px solid #f5ffff; border-radius:4px; width:100%; max-width:578px; mso-border-alt: none;">';
-  html += '<tbody><tr style="border:1px solid #d5ecff; mso-border-alt:none; display:block; border-radius: 3px;">';
-  html += '<td style="display:block; padding:8px; border-radius:2px; border:1px solid #99b0e1; vertical-align:top; background-color:white; mso-border-alt:none;">';
+  try {
+    var html = '<style>.card-label{color: white; font-size: 0.7em; border-radius:3px;-moz-box-sizing:border-box;box-sizing:border-box;display:block;float:left;height:30px;line-height:30px;margin:0 4px 4px 0;min-width:40px;padding:0 10px;width:auto;}  .card-label-green{background-color:#61bd4f}.card-label-yellow{background-color:#f2d600}.card-label-orange{background-color:#ffab4a}.card-label-red{background-color:#eb5a46} .card-label-purple{background-color:#c377e0} .card-label-blue{background-color:#0079bf} .card-label-pink{background-color:#ff80ce}.card-label-sky{background-color:#00c2e0}.card-label-lime{background-color:#51e898}.card-label-black{background-color:#4d4d4d}</style>';
+    html += '<table id="" class="card-v3" cellpadding="0" cellspacing="0" style="border:1px solid #f5ffff; border-radius:4px; width:100%; max-width:578px; mso-border-alt: none;">';
+    html += '<tbody><tr style="border:1px solid #d5ecff; mso-border-alt:none; display:block; border-radius: 3px;">';
+    html += '<td style="display:block; padding:8px; border-radius:2px; border:1px solid #99b0e1; vertical-align:top; background-color:white; mso-border-alt:none;">';
 
-  html += '<div style="font-family: Helvetica Neue, Arial, Helvetica, sans-serif;">'
-  html += '<h2>' + response.body.name + '</h2>';
+    html += '<div style="font-family: Helvetica Neue, Arial, Helvetica, sans-serif;">'
+    html += '<h2>' + response.body.name + '</h2>';
 
-  for (label of response.body.labels) {
-    html += '<div class="card-label card-label-' + label.color + '">&nbsp;' + label.name + '</div>';
+    if (response.body.labels && response.body.labels.length > 0) {
+      for (label of response.body.labels) {
+        html += '<div class="card-label card-label-' + label.color + '">&nbsp;' + label.name + '</div>';
+      }
+      html += '<br/>';
+    }
+
+    html += '<h5>' + response.body.desc + '</h5>';
+    html += '<p style="text-align: right; font-size: 0.7em">trello.com</p>';
+    html += '</div>';
+
+    html += '</td> </tr> </tbody> </table>';
+  } catch (e) {
+    res.status(500).send('Error');
+    return;
   }
-
-  html += '<br/><h5>' + response.body.desc + '</h5>';
-  html += '<p style="text-align: right; font-size: 0.7em">trello.com</p>';
-  html += '</div>';
-
-  html += '</td> </tr> </tbody> </table>';
 
   res.json({
     body: html
